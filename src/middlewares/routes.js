ae9d9a31e5f0425b5aa7c2132805a1c0268bb3e0
yuribodo <yemotalara@gmail.com>
2025-01-04 22:49:16 -0400

    feat: add a functional POST route to tasks

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -1,6 +1,6 @@
 import { buildRoutePath } from "../utils/build-route-path.js";
 import { Database } from "./database.js";
-import {randomUUID} from "node:crypto";
+import { randomUUID } from "node:crypto";
 
 const database = new Database();
 
@@ -9,7 +9,7 @@ export const routes = [
         method: 'POST',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
-            const {title, description} = req.body
+            const { title, description } = req.body
 
             const task = {
                 id: randomUUID(),
@@ -29,9 +29,9 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
-            const {search} = req.query
+            const { search } = req.query
 
-            const tasks = database.select('tasks', search? {
+            const tasks = database.select('tasks', search ? {
                 title: search,
                 description: search,
                 completed_at: search,
@@ -46,43 +46,45 @@ export const routes = [
         method: 'PUT',
         path: buildRoutePath('/tasks/:id'),
         handler: (req, res) => {
-            const {search} = req.query
+            const { id } = req.params
+            const { title, description } = req.body
 
-            const tasks = database.select('tasks', search? {
-                title: search,
-                description: search,
-                completed_at: search,
-                created_at: search,
-                updated_at: search,
-            } : null);
+            const task = database.selectById("tasks", id )
 
-            return res.end(JSON.stringify(tasks));
+            if (!task) {
+                return res.status(404).end('Task not foud')
+            }
+
+            const updatedTask = {
+                ...task,
+                updated_at: new Date(),
+                ...(title && {title}),
+                ...(description && {description})
+            }
+
+            database.update('tasks', id, updatedTask)
+
+            return res.writeHead(204).end()
         },
     },
     {
         method: 'DELETE',
         path: buildRoutePath('/tasks/:id'),
         handler: (req, res) => {
-            const {search} = req.query
+            const { id } = req.params
 
-            const tasks = database.select('tasks', search? {
-                title: search,
-                description: search,
-                completed_at: search,
-                created_at: search,
-                updated_at: search,
-            } : null);
+            database.delete('tasks', id)
 
-            return res.end(JSON.stringify(tasks));
+            return res.writeHead(204).end()
         },
     },
     {
         method: 'PATCH',
         path: buildRoutePath('/tasks/:id/complete'),
         handler: (req, res) => {
-            const {search} = req.query
+            const { search } = req.query
 
-            const tasks = database.select('tasks', search? {
+            const tasks = database.select('tasks', search ? {
                 title: search,
                 description: search,
                 completed_at: search,
